5c058498da8ddec842fb720f861d900cb9ceea39
Bartek Welter <welterbartek8@gmail.com>
2018-08-23 19:30:18 +0200

    add builder

--- src/main/java/com/sda/Builder/Main.java
+++ src/main/java/com/sda/Builder/Main.java
@@ -3,5 +3,11 @@ package com.sda.Builder;
 public class Main {
     public static void main(String[] args) {
 
+        User user = User.UserBuilder.anUser()
+                .age(21)
+                .email("szogun@cdprojekt.com")
+                .build();
+
+        System.out.println(user.getAge());
     }
 }
